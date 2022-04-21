4a04556b6a63e60edbab2a868309fa458563084c
vaisakh <bvaisakh@korewireless.com>
2022-04-21 06:58:35 +0530

    01_exercise (wip)

--- 01_exercise/sort.cpp
+++ 01_exercise/sort.cpp
@@ -1,12 +1,65 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char** argv)
+void printCharArray(int size, char **argv);
+double *createDoubleArrayFromArgv(int size, char **argv);
+void printDoubleArray(int size, double *doublePtr);
+int compare(const void *x, const void *y);
+
+int main(int argc, char **argv)
 {
-  cout << "You have entered " << argc << " arguments" << "\n";
+  double *doubleArray;
+  int size = argc - 1;
 
-  for (int i = 0; i < argc; i++) 
-    cout << argv[i] << "\n";
+  cout << endl
+       << "Printing Input" << endl;
+  cout << "--------------" << endl;
+  printCharArray(size, argv);
 
-  return 0
+  doubleArray = createDoubleArrayFromArgv(size, argv);
+
+  cout << endl
+       << "Printing Unsorted Array" << endl;
+  cout << "---------------------" << endl;
+  printDoubleArray(size, doubleArray);
+
+  qsort(doubleArray, size, sizeof(double), compare);
+
+  cout << endl
+       << "Printing Sorted Array" << endl;
+  cout << "---------------------" << endl;
+  printDoubleArray(size, doubleArray);
+
+  free(doubleArray);
+  return 0;
+}
+
+void printCharArray(int size, char **argv)
+{
+  cout << "You have entered " << size << " arguments"
+       << endl;
+
+  for (int i = 0; i < size; i++)
+    cout << argv[i + 1] << endl;
+}
+
+double *createDoubleArrayFromArgv(int size, char **argv)
+{
+  double *doublePtr = (double *)malloc(size * sizeof(double));
+
+  for (int i = 0; i < size; i++)
+    *(doublePtr + i) = stod(argv[(i + 1)]);
+
+  return doublePtr;
+}
+
+void printDoubleArray(int size, double *doublePtr)
+{
+  for (int i = 0; i < size; i++)
+    cout << doublePtr[i] << endl;
+}
+
+int compare(const void *x, const void *y)
+{
+  return (*(double *)x - *(double *)y);
 }
